554ff7973492b8cd4ad31a18992ab380974313c3
Sathira Williams <sathira.williams@gmail.com>
2024-01-22 20:17:34 -0500

    (Initial) Skeleton website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pwWebsite';
+  title = 'polywertz';
+  showLinks= false;
+  toggleLinks(){
+    this.showLinks = !this.showLinks;
+  }
 }
+
+
+
